cab6f70f9ed70216dc55affc3eb9bbbd099da1d1
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-14 23:25:28 +0530

    15 problem

---
+++ 16.js
@@ -0,0 +1,15 @@
+//* https://leetcode.com/problems/promise-time-limit/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+
+        const onSuccess = fn(...args)
+        const timeoutPromise = new Promise((resolve, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t)
+        })
+
+        return Promise.race([onSuccess, timeoutPromise])
+        
+    }
+};
