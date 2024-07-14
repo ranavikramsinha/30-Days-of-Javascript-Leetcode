6fa30ed2cc114032f047d093ae02c50e8836fcb5
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-14 22:34:35 +0530

    01 problem

---
+++ 01.js
@@ -0,0 +1,10 @@
+//* https://leetcode.com/problems/create-hello-world-function/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+var createHelloWorld = function() {
+    
+    return function(...args) {
+
+        return "Hello World"
+        
+    }
+};
